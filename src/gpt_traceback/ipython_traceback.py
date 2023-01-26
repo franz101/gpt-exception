b7d59de416612382dff6a2670ed50d1df3a63969
franz.media <fkrekeler@gmail.com>
2023-01-26 02:06:33 +0100

    add rich error printing

--- src/gpt_traceback/ipython_traceback.py
+++ src/gpt_traceback/ipython_traceback.py
@@ -1,37 +1,25 @@
 import IPython, inspect
+import textwrap
 from IPython.core import ultratb
 import traceback
 from .prompt import predict_prompt
+from .print_exception import print_exception
 
 def exception_hook(exc_type, exc_value, exc_traceback):
-    error = "Hello stack overflow, I need help I ran into the following error in my notebook:\n"
-    exception = "\n".join(traceback.format_exception_only(exc_type, exc_value))
-    error += "Exception type and exception value:\n" + exception + "\n"
-    print(exception)
-    traceback_str ="\n".join(traceback.format_tb(exc_traceback, 2))
-    error += "Traceback (2):\n" + traceback_str + "\n"
-    print(traceback_str)
-    #print("\n".join(IPython.get_ipython().history_manager.get_range(-1,0)))
-    # TODO: print the cell where the error happened and the previous cells of it
     last_tb = exc_traceback
     while last_tb.tb_next:
         last_tb = last_tb.tb_next
-    f = last_tb.tb_frame
-    lineno = last_tb.tb_lineno
-    co = f.f_code
-    filename = co.co_filename
-    function_name = co.co_name
-    source = inspect.getsource(f)
-    print(f'File Name: {filename}, Function Name: {function_name}, Line No: {lineno}')
-    print(source)
+    source = inspect.getsource(last_tb.tb_frame)
+    print_exception(exc_type, exc_value, exc_traceback)
+    error = "Hello stack overflow, I need help I ran into the following error in my notebook:\n"
+    exception = "\n".join(traceback.format_exception_only(exc_type, exc_value))
+    error += "Exception type and exception value:\n" + exception + "\n"
+    traceback_str ="\n".join(traceback.format_tb(exc_traceback, 2))
+    error += "Traceback (2):\n" + traceback_str + "\n"
     error += "Source of cell:\n" + source + "\nThe solution to the exception is:"
-    print("predicting exception...")
-    print("...")
-    print(predict_prompt(error))
-    #linecache.checkcache(filename)
-    #line = linecache.getline(filename, lineno, f.f_globals)
-    #print(f'File Name: {filename}, Function Name: {function_name}, Line No: {lineno}, Line Content: {line.strip()}')
-
+    formatted_string = textwrap.wrap(predict_prompt(error), width=60)
+    print("\n".join(formatted_string))
+    
 def custom_exc(shell, etype, evalue, tb, tb_offset=None):
   lshell = shell
   ltb_offset = tb_offset
---
+++ src/gpt_traceback/print_exception.py
@@ -0,0 +1,38 @@
+from rich.traceback import  Traceback
+from rich.console import Console
+
+console = Console()
+
+def print_exception(exc_type, exc_value, exc_traceback):
+    tb_offset = 0
+    ltb = tb
+    for _ in range(tb_offset):
+        if ltb is None:
+            break
+        ltb = ltb.tb_next
+    locals_hide_sunder = None
+    locals_hide_sunder = (
+        True
+        if (console.is_jupyter and locals_hide_sunder is None)
+        else locals_hide_sunder
+    )
+    traceback_exception = Traceback.from_exception(
+                exc_type,
+                exc_value,
+                exc_traceback,
+                width = 100,
+    extra_lines = 3,
+    theme= None,
+    word_wrap = False,
+    show_locals =  False,
+    locals_max_length =  10,
+    locals_max_string =  80,
+    locals_hide_dunder =  True,
+    locals_hide_sunder =  locals_hide_sunder,
+    indent_guides =  True,
+    suppress =   (),
+    max_frames =  100,
+            )
+    console.print(
+            traceback_exception
+        )
