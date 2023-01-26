35a1c4eafcf42a3a15009de21fbde4029f817a87
franz.media <fkrekeler@gmail.com>
2023-01-26 01:19:35 +0100

    fix test

--- tests/test_prompt.py
+++ tests/test_prompt.py
@@ -6,7 +6,9 @@ from gpt_traceback.prompt import predict_prompt
 class TestSimple(unittest.TestCase):
 
     def test_add(self):
-        self.assertNotEqual(predict_prompt, None)
+        def iscallable():
+            return callable(predict_prompt)
+        self.assertEqual(iscallable, True)
 
 
 if __name__ == '__main__':
