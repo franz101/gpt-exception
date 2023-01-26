995b28367de565ff7bc1beb8d097c47df5e5d846
franz.media <fkrekeler@gmail.com>
2023-01-26 01:16:19 +0100

    fix warnings

--- src/gpt_traceback/__init__.py
+++ src/gpt_traceback/__init__.py
@@ -1,8 +1,8 @@
 __version__ = "0.1.0"
 
-import warning
+import warnings
 
 try:
     from .ipython_traceback import *
 except ImportError:
-    warning.warn("IPython could not be imported, are you in Google Colab?")
+    warnings.warn("IPython could not be imported, are you in Google Colab?")
